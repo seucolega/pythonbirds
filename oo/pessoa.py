e6f3f1c8741e223349b406e2c9cae15ebbb77d78
Gustavo Sousa <gustavosousa@jeporu.com>
2019-08-17 12:43:39 -0300

    Estudo com métodos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,14 +9,17 @@ class Pessoa:
     def cumprimento(self):
         return f'Olá {self.nome}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 
 if __name__ == '__main__':
     p1 = Pessoa(nome='José')
     p2 = Pessoa(p1, nome='Maria')
-    print(Pessoa.olhos, Pessoa.__dict__)
-    print(p1.olhos, p1.__dict__)
-    p1.olhos = 1
-    print(p1.olhos, p1.__dict__)
-    Pessoa.olhos = 3
-    print(p1.olhos, p1.__dict__)
-    print(p2.olhos, p2.__dict__)
+    print(Pessoa.metodo_estatico(), p1.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), p1.nome_e_atributos_de_classe())
